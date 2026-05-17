4ebb5ebd949990978d26055cb0a6f9264dcf3b3b
Masha <nasonkina@yandex.ru>
2026-05-17 23:52:33 +0300

    fix api rm logs

--- src/utils/burger-api.ts
+++ src/utils/burger-api.ts
@@ -3,8 +3,6 @@ import { TIngredient, TOrder, TOrdersData, TUser } from './types';
 
 const URL = process.env.BURGER_API_URL;
 
-console.log(`process.env.BURGER_API_URL: ${process.env.BURGER_API_URL}`);
-
 const checkResponse = <T>(res: Response): Promise<T> =>
   res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
 
