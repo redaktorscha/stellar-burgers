ff512c05627a9f2d8fd767c8adb818599ee3b04b
Masha <nasonkina@yandex.ru>
2026-06-12 16:19:28 +0300

    add cypress config

---
+++ cypress.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'webpack'
+    }
+  },
+
+  e2e: {
+    baseUrl: 'http://localhost:4000/',
+    supportFile: false
+  }
+});
